9439d7ebd528b0ff33a081e87b82267185faabff
tuyenphann <tuyenphan@kms-technology.com>
2022-08-17 01:27:04 +0700

    Make changes

--- functions/index.ts
+++ functions/index.ts
@@ -4,7 +4,7 @@ import express from 'express';
 const app = express();
 app.get('/', (req, res) => res.status(200).send('Hey there!!!!'));
 
-const PORT = Number(process.env.PORT) || 8080;
+const PORT = 8080;
 app.listen(PORT, () => {
   console.log(`App listing on port ${PORT}`);
 });
