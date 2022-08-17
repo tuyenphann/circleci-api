83715b74dc5791f898ebecb454ef0e582ba44dc0
tuyenphann <tuyenphan@kms-technology.com>
2022-08-18 00:41:10 +0700

    Make changes

--- functions/db/migrations/20220815171508_create-tables.ts
+++ functions/db/migrations/20220815171508_create-tables.ts
@@ -29,7 +29,6 @@ export async function up(knex: Knex): Promise<void> {
       table.unique(['userId', 'serviceProviderId']);
       table.timestamp('joiningDate', { useTz: true }).notNullable();
       table.timestamp('updatedOn', { useTz: true });
-      table.timestamp('createdOn', { useTz: true });
     });
 }
 
