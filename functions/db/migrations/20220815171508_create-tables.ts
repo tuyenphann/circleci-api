a5bd619d1af4992024e2012db25711a23c06d8fa
tuyenphann <tuyenphan@kms-technology.com>
2022-08-18 13:12:43 +0700

    Make changes

--- functions/db/migrations/20220815171508_create-tables.ts
+++ functions/db/migrations/20220815171508_create-tables.ts
@@ -28,6 +28,8 @@ export async function up(knex: Knex): Promise<void> {
         .onDelete('RESTRICT');
       table.unique(['userId', 'serviceProviderId']);
       table.timestamp('joiningDate', { useTz: true }).notNullable();
+      table.timestamp('createdOn', { useTz: true });
+      table.timestamp('updatedOn', { useTz: true });
     });
 }
 
